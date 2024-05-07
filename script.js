55bc36d4d38b48e784f88fd087c09cff5be1c2ad
OntongGhebi <ghebiarmando@gmail.com>
2024-05-08 01:26:04 +0700

    js close navbar with click outside of navbar

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.querySelector('#hamburger-menu').
 onclick = () => {
     navbarNav.classList.toggle('active');
 };
+
+//Klik out sidebar for close the sidebar//
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
